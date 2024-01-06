a5e4c0bd5fb559e340ac07bfa024a6dbb5de7220
rayhanasyraff <rayhanasyraffhjamran@gmail.com>
2024-01-06 17:42:33 +0800

    Change path

--- api/serverless.js
+++ api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../dist/putra-t-api/main.js"));
+app.register(import("../src/app/app.ts"))
 
 export default async (req, res) => {
     await app.ready();
